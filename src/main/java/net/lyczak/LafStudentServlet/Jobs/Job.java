669b57ea8bc08e24c435df214e829e080dc2a32d
Delta Lyczak <dlyczak@protonmail.com>
2021-04-04 14:10:52 -0400

    Add Job id

--- src/main/java/net/lyczak/LafStudentServlet/Jobs/Job.java
+++ src/main/java/net/lyczak/LafStudentServlet/Jobs/Job.java
@@ -3,13 +3,19 @@ package net.lyczak.LafStudentServlet.Jobs;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public abstract class Job<T, E> implements Runnable {
+    private static int lastId = 1;
+
     protected RemoteWebDriver driver;
     protected boolean repeating;
+    protected int id;
     protected JobResult<T, E> result = JobResult.pending();
 
     public Job(RemoteWebDriver driver, boolean repeating) {
         this.driver = driver;
         this.repeating = repeating;
+
+        lastId++;
+        this.id = lastId;
     }
 
     public RemoteWebDriver getDriver() {
@@ -26,6 +32,10 @@ public abstract class Job<T, E> implements Runnable {
 
     public void setRepeating(boolean repeating) {
         this.repeating = repeating;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public JobResult<T, E> getResult() {
