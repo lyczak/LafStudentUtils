5ee2abf9f77d689d23078f1e3216cce9eb0ca86b
Delta Lyczak <dlyczak@protonmail.com>
2021-04-03 22:57:50 -0400

    Add MoodleEventsJob with ex handling

---
+++ src/main/java/net/lyczak/LafStudentServlet/Jobs/MoodleEventsJob.java
@@ -0,0 +1,35 @@
+package net.lyczak.LafStudentServlet.Jobs;
+
+import net.lyczak.LafStudentUtils.LsuException;
+import net.lyczak.LafStudentUtils.Models.MoodleEvent;
+import net.lyczak.LafStudentUtils.MoodleClient;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MoodleEventsJob extends Job<List<MoodleEvent>, LsuException> {
+    private MoodleClient moodle;
+
+    public MoodleEventsJob(RemoteWebDriver driver, boolean repeating) {
+        super(driver, repeating);
+    }
+
+    @Override
+    public void run() {
+        try {
+            List<MoodleEvent> events = moodle.getEvents(driver, driver);
+
+            if (events != null) {
+                result = JobResult.ok(events);
+            } else {
+                result = JobResult.error(new LsuException("Events list was null", driver.getPageSource()));
+            }
+        } catch (LsuException e) {
+            result = JobResult.error(e);
+        } catch (Exception e) {
+            result = JobResult.error(new LsuException(e.getMessage(),
+                    driver.getPageSource()).withDetail(Arrays.deepToString(e.getStackTrace())));
+        }
+    }
+}
